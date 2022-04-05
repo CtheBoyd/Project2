91ecfb6bcd1e0c13cdc89ac5e0352079f4f89cc3
CtheBoyd <99106917+CtheBoyd@users.noreply.github.com>
2022-04-05 15:49:26 -0700

    update

---
+++ change.py
@@ -0,0 +1,8 @@
+# Author: Christopher Boyd
+# GitHub username: CtheBoyd
+# Date: 4/4/2022
+# Description: Asks user to provide an amount under one dollar and returns the change required.
+#
+print ("Please enter an amount in cents less than a dollar.")
+num_1 = float(input())
+print ("Your change will be:")
--- temp_convert.py
+++ temp_convert.py
@@ -1,3 +1,8 @@
+# Author: Christopher Boyd
+# GitHub username: CtheBoyd
+# Date: 4/4/2022
+# Description: Asks user to provide temerature in Celcius and returns it in Fahrenheit.
+#
 print ("Please enter a Celsius temperature.")
 num_1 = float(input())
 print ("The equivalent Fahrenheit temperature is:")
