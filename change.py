16b9b6ba1179c829d854077724bec81ccf9352ae
CtheBoyd <99106917+CtheBoyd@users.noreply.github.com>
2022-04-06 14:52:44 -0700

    update

--- change.py
+++ change.py
@@ -1,8 +1,19 @@
-# Author: Christopher Boyd
+#5 Author: Christopher Boyd
 # GitHub username: CtheBoyd
-# Date: 4/4/2022
+# Date: 4/5/2022
 # Description: Asks user to provide an amount under one dollar and returns the change required.
 #
 print ("Please enter an amount in cents less than a dollar.")
-num_1 = float(input())
+change = float(input())
+q = (change // 25)
+qr = (change % 25)
+d = (qr // 10)
+dr = (qr % 10)
+n = (dr // 5)
+nr = (dr % 5)
+p = (nr // 1)
 print ("Your change will be:")
+print ("Q:", q)
+print ("D:", d)
+print ("N:", n)
+print ("P:", p)
